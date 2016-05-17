2479c967608bee7e18601ad2ea0d528a5543d4c2
Ken Koh <k.koh@meta-view.com>
2016-05-17 13:56:33 +1000

    Missed file

---
+++ Assets/ToggleableIcon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleableIcon : MonoBehaviour {
+
+    public Sprite offSprite;
+    public Sprite onSprite;
+
+    private Image _img;
+    private bool _isOn = false;
+
+    void Start()
+    {
+        _img = GetComponent<Image>();
+    }
+
+    public void Toggle()
+    {
+        if (_isOn)
+        {
+            _img.sprite = offSprite;
+        }
+        else
+        {
+            _img.sprite = onSprite;
+        }
+        _isOn = _isOn ? false : true;
+    }
+}
